17a1278bce46a821d5c4b00161573177ddae2cea
Petar Palasek <ppalasek@gmail.com>
2022-01-15 14:47:37 +0000

    change order and add log scale generate script

--- do_all.sh
+++ do_all.sh
@@ -8,12 +8,15 @@ Rscript generate_region_data.r
 Rscript generate_cases_line_plots.r
 Rscript generate_age_plot_lines.r
 Rscript generate_cases_hosp_deaths.r
+Rscript generate_cases_hosp_deaths_log.r
 
+Rscript generate_age_plot_moving_dots_per_county.r
+
+Rscript generate_vacc_map.r
 Rscript generate_map_plots.r
 
 Rscript generate_age_plot_moving_dots.r
 Rscript generate_age_plot_moving_dots_last_year.r
-Rscript generate_age_plot_moving_dots_per_county.r
 
 Rscript generate_age_plot.r
 Rscript generate_age_plot_all_data.r
@@ -22,7 +25,6 @@ python fit_exp_all.py
 python fit_exp_all_deaths_and_hospitalisations.py
 
 Rscript generate_pyramid_plot.r
-Rscript generate_vacc_map.r
 
 Rscript generate_html.r
 
